66cff180679522c45b2c509ff0957226f99e9f20
SanskarRaut <94779936+SanskarRaut@users.noreply.github.com>
2022-06-27 16:33:08 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,26 +1,15 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"  
-    pageEncoding="ISO-8859-1"%>  
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">  
-<html>  
-<head>  
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">  
-<title>Insert title here</title>  
-</head>  
-<body>  
-  
-<%  
-int n = 10, firstTerm = 0, secondTerm = 1;
-    System.out.println("Fibonacci Series till " + n + " terms:");
-
-    for (int i = 1; i <= n; ++i) {
-         .out.print(firstTerm + ", ");
-
-      int nextTerm = firstTerm + secondTerm;
-      firstTerm = secondTerm;
-      secondTerm = nextTerm;
-    } 
-  
-%>  
-  
-</body>  
-</html> 
+public class Fibonacci{
+   public static void main(String args[]) {
+      int a, b, c, i, n;
+      n = 10;
+      a = b = 1;
+      System.out.print(a+" "+b);
+      for(i = 1; i <= n-2; i++) {
+         c = a + b;
+         System.out.print(" ");
+         System.out.print(c);
+         a = b;
+         b = c;
+      }
+   }
+}
